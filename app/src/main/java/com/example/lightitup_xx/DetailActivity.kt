e30c2a278f6dc88cd4bb28a117097210c583c2ba
yerining <dyoerr9030@naver.com>
2021-08-09 18:00:32 +0900

    [mod] backbutton

--- app/src/main/java/com/example/lightitup_xx/DetailActivity.kt
+++ app/src/main/java/com/example/lightitup_xx/DetailActivity.kt
@@ -1,11 +1,11 @@
 package com.example.lightitup_xx
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_details)
     }
 }
